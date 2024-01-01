3fd600f7b2a875a4227bd2911ee005c58d1b206c
uliknurdiansah <uliknurdiansah@gmail.com>
2023-12-31 22:08:46 -0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>uji coba git comit dari git</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cupiditate recusandae placeat et consequatur non libero amet animi nihil harum voluptatem, laudantium blanditiis mollitia numquam voluptates voluptatibus fugiat neque sint.</p>
 </body>
+
 </html>
